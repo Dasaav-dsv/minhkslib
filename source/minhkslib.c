227317406301e189c04f14e74ad65f211e6dbef3
Dasaav <111015245+Dasaav-dsv@users.noreply.github.com>
2023-12-23 18:48:33 +0100

    minlualib -> minhkslib

--- source/minhkslib.c
+++ source/minhkslib.c
@@ -4,7 +4,7 @@
 #include "../include/console.h"
 #include "../include/hkslua.h"
 
-int __declspec(dllexport) luaopen_minlualib(lua_State* L) {
+int __declspec(dllexport) luaopen_minhkslib(lua_State* L) {
     openConsole();
     const luaL_reg* exports = getLuaExports();
     hksI_openlib_fn f = NULL;
